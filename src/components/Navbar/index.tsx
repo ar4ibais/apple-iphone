866e92c16dd39b57a4d65cacd50d50ed49e833ed
Arthur <ak.hutov@yandex.ru>
2024-08-07 14:36:47 +0400

    first commit

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,33 @@
+import { navLists } from "../../constants";
+import { appleImg, bagImg, searchImg } from "../../utils";
+
+const Navbar = () => {
+    return (
+        <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+            <nav className="flex w-full screen-max-width">
+                <img src={appleImg} alt="Apple logo" width={18} height={18} />
+                <div className="flex flex-1  justify-center max-sm:hidden">
+                    {navLists.map((item, index) => (
+                        <div
+                            key={index}
+                            className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all duration-300"
+                        >
+                            {item}
+                        </div>
+                    ))}
+                </div>
+                <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+                    <img
+                        src={searchImg}
+                        alt="Search icon"
+                        width={18}
+                        height={18}
+                    />
+                    <img src={bagImg} alt="Bag icon" width={18} height={18} />
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
